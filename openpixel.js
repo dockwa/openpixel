7ada0331f8d78255dabf5431f529d918c74c59ad
Stuart Yamartino <stuart@yamartino.com>
2016-04-12 23:47:01 -0400

    Added 3 day cache busting for loading snippet js script

--- openpixel.js
+++ openpixel.js
@@ -235,11 +235,11 @@ var Controller = {
         // set guid for user for 2 years
         Cookie.set('uid', guid(), 2 * 365 * 24 * 60);
       }
-    } else if (value == 'pageview') {
-      if (!Config.pageViewOnce && !Cookie.exists('pageview')) {
+    } else if (value == 'pageload') {
+      if (!Config.pageViewOnce && !Cookie.exists('pageload')) {
         Config.pageViewOnce = true;
         // set 10 minutes page view cookie
-        Cookie.set('pageview', 'true', 10, window.location.pathname);
+        Cookie.set('pageload', 'true', 10, window.location.pathname);
         new Pixel(value, pixelFunc.t);
       }
     } else if (event == 'event' && value != 'pageclose') {
